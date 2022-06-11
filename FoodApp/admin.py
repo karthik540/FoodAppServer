15911fdcf0a4a894dc94a32f0ac53be3e85e417e
karthik540 <rajkarthik967@gmail.com>
2022-06-12 04:57:43 +0530

    changes added

--- FoodApp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Food, Order, Table
-
-admin.site.register(Food)
-admin.site.register(Table)
-admin.site.register(Order)
---
+++ Procfile
@@ -0,0 +1,3 @@
+release: python manage.py migrate
+
+web: gunicorn Beatsite_Django.wsgi --log-file -
